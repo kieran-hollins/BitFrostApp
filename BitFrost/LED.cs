a20b3ba9e21ac51b25db81927b68e0fea6523a38
Kieran Hollins <khollins123@gmail.com>
2024-05-03 16:45:26 +0100

    Add project files.

---
+++ BitFrost/LED.cs
@@ -0,0 +1,98 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+
+namespace BitFrost
+{
+    public class LED
+    {
+        public int StartDMXAddress { get; set; }
+        public LEDProfile LEDProfile { get; set; }
+
+        public LED(int startDMXAddress, LEDProfile ledProfile)
+        {
+            StartDMXAddress = startDMXAddress;
+            LEDProfile = ledProfile;
+        }
+    }
+
+
+    public abstract class LEDProfile
+    {
+        public abstract int Channels { get; }
+        public abstract string Type { get; }
+    }
+
+    public class RGB : LEDProfile
+    {
+        public override int Channels => 3;
+        public override string Type => "RGB";
+
+        public int Red { get; set; }
+        public int Green { get; set; }
+        public int Blue { get; set; }
+        
+        public void ConvertColour(int r, int g, int b)
+        {
+            Red = Clamp(r);
+            Green = Clamp(g);
+            Blue = Clamp(b);
+        }
+
+        private static int Clamp(int input)
+        {
+            if (input < 0) return 0;
+            if (input > 255) return 255;
+            else return input;
+        }
+    }
+
+    public class RGBW : LEDProfile
+    {
+        public override int Channels => 4;
+        public override string Type => "RGBW";
+
+        public int Red { get; set; }
+        public int Green { get; set; }
+        public int Blue { get; set; }
+        public int White { get; set; }
+
+        public void ConvertColour(int r, int g, int b, int w)
+        {
+            Red = Clamp(r);
+            Green = Clamp(g);
+            Blue = Clamp(b);
+            White = Clamp(w);
+        }
+
+        private static int Clamp(int input)
+        {
+            if (input < 0) return 0;
+            if (input > 255) return 255;
+            else return input;
+        }
+    }
+
+    public class GRB : LEDProfile
+    {
+        public override int Channels => 3;
+        public override string Type => "GRB";
+
+        public int Green { get; set; }
+        public int Red { get; set; }
+        public int Blue { get; set; }
+
+        public void ConvertColour(int r, int g, int b)
+        {
+            Green = Clamp(g);
+            Red = Clamp(r);
+            Blue = Clamp(b);
+        }
+
+        private static int Clamp(int input)
+        {
+            if (input < 0) return 0;
+            if (input > 255) return 255;
+            else return input;
+        }
+    }
+}
---
+++ BitFrost/LightingPatch.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace BitFrost
+{
+    public class LightingPatch
+    {
+        private Dictionary<(int x, int y), LED> patch;
+        private Dictionary<int, (int x, int y)> dmxAddressMap;
+
+        public LightingPatch()
+        {
+            patch = new Dictionary<(int x, int y), LED> ();
+            dmxAddressMap = new Dictionary<int, (int x, int y)> ();
+        }
+
+        public void AddLED(int x, int y, LED led)
+        {
+            var coordinates = (x, y);
+            var startDMXAddress = led.StartDMXAddress;
+
+            if (patch.ContainsKey(coordinates))
+            {
+                throw new ArgumentException($"LED already existing at coordinates ({x}, {y}).");
+            }
+
+            for (int i = startDMXAddress; i < startDMXAddress + led.LEDProfile.Channels; i++)
+            {
+                if (dmxAddressMap.ContainsKey(i))
+                {
+                    throw new ArgumentException($"DMX address {i} is already in use.");
+                }
+            }
+
+            patch.Add(coordinates, led);
+            for (int i = startDMXAddress; i < startDMXAddress + led.LEDProfile.Channels; i++)
+            {
+                dmxAddressMap.Add(i, coordinates);
+            }
+        }
+
+        public void RemoveLED(int x, int y)
+        {
+            var coordinates = (x, y);
+
+            if (!patch.ContainsKey(coordinates))
+            {
+                throw new ArgumentException($"No LED at coordinates ({x}, {y})");
+            }
+
+            int startDMXAddress = GetStartDMXChannel(x, y);
+            var led = patch[coordinates];
+
+            for (int i = startDMXAddress; i < startDMXAddress + led.LEDProfile.Channels; i++)
+            {
+                dmxAddressMap.Remove(i);
+            }
+
+            patch.Remove(coordinates); 
+        }
+
+        private int GetStartDMXChannel(int x, int y)
+        {
+            var coordinates = (x, y);
+            var led = patch[coordinates];
+
+            return led.StartDMXAddress;
+        }
+    }
+}
