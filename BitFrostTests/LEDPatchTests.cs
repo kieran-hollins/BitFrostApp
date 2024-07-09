bc31caf8160921b46bddf74589b38faf40e49b20
Kieran Hollins <khollins123@gmail.com>
2024-07-09 21:29:39 +0100

    Added more tests to the library

--- BitFrostTests/LEDPatchTests.cs
+++ BitFrostTests/LEDPatchTests.cs
@@ -74,5 +74,94 @@ namespace BitFrostTests
 
         }
 
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddLED_SameLocation_ThrowsException()
+        {
+            LightingPatch patch = LightingPatch.Instance;
+            patch.ClearAll();
+
+            int x = 0, y = 0;
+            int address = 1;
+            RGB type = new();
+            LED led1 = new(address, type);
+            LED led2 = new(address, type);
+
+            patch.AddLED(x, y, led1);
+            patch.AddLED(x, y, led2); // Should throw exception
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddLED_SameDMXAddress_ThrowsException()
+        {
+            LightingPatch patch = LightingPatch.Instance;
+            patch.ClearAll();
+
+            int x1 = 0, y1 = 0;
+            int x2 = 1, y2 = 0;
+            int address = 1;
+            RGB type = new();
+            LED led1 = new(address, type);
+            LED led2 = new(address, type);
+
+            patch.AddLED(x1, y1, led1);
+            patch.AddLED(x2, y2, led2); // Should throw exception
+        }
+
+        [TestMethod]
+        public void RemoveLED_ValidLocation_RemovesLED()
+        {
+            LightingPatch patch = LightingPatch.Instance;
+            patch.ClearAll();
+
+            int x = 0, y = 0;
+            int address = 1;
+            RGB type = new();
+            LED led = new(address, type);
+
+            patch.AddLED(x, y, led);
+            patch.RemoveLED(x, y);
+
+            try
+            {
+                patch.GetLEDLocation(address); // Should throw exception
+                Assert.Fail("Expected ArgumentException not thrown");
+            }
+            catch (ArgumentException) { }
+        }
+
+        [TestMethod]
+        public void ClearAll_ClearsAllLEDs()
+        {
+            LightingPatch patch = LightingPatch.Instance;
+            patch.ClearAll();
+
+            int x = 0, y = 0;
+            int address = 1;
+            RGB type = new();
+            LED led = new(address, type);
+
+            patch.AddLED(x, y, led);
+            patch.ClearAll();
+
+            Assert.AreEqual(0, patch.GetTotalLEDs());
+        }
+
+        [TestMethod]
+        public void AddRGBLEDLineHorizontal_AddsCorrectNumberOfLEDs()
+        {
+            LightingPatch patch = LightingPatch.Instance;
+            patch.ClearAll();
+
+            int x = 0, y = 0;
+            int startAddress = 1;
+            int quantity = 5;
+
+            patch.AddRGBLEDLineHorizontal(x, y, startAddress, quantity);
+
+            Assert.AreEqual(quantity * new RGB().Channels, patch.GetTotalLEDs());
+        }
+
     }
 }
--- BitFrostTests/LEDTests.cs
+++ BitFrostTests/LEDTests.cs
@@ -6,13 +6,71 @@ namespace BitFrostTests
     public class LEDTests
     {
         [TestMethod]
-        public void CheckRGBLEDChannels()
+        public void CreateRGBLED_ValidAddress_CreatesLED()
         {
-            RGB rgbType = new RGB();
-            LED led = new(1, rgbType);
+            int address = 1;
+            LED led = LED.CreateRGBLED(address);
 
-            Console.WriteLine("Number of channels expected: 3");
-            Console.WriteLine($"Number of channels found: {led.LEDProfile.Channels}");
+            Assert.AreEqual(address, led.StartDMXAddress);
+            Assert.AreEqual(3, led.LEDProfile.Channels);
+            Assert.IsInstanceOfType(led.LEDProfile, typeof(RGB));
+        }
+
+        [TestMethod]
+        public void CreateGRBLED_ValidAddress_CreatesLED()
+        {
+            int address = 1;
+            LED led = LED.CreateGRBLED(address);
+
+            Assert.AreEqual(address, led.StartDMXAddress);
+            Assert.AreEqual(3, led.LEDProfile.Channels);
+            Assert.IsInstanceOfType(led.LEDProfile, typeof(GRB));
+        }
+
+        [TestMethod]
+        public void CreateRGBWLED_ValidAddress_CreatesLED()
+        {
+            int address = 1;
+            LED led = LED.CreateRGBWLED(address);
+
+            Assert.AreEqual(address, led.StartDMXAddress);
+            Assert.AreEqual(4, led.LEDProfile.Channels);
+            Assert.IsInstanceOfType(led.LEDProfile, typeof(RGBW));
+        }
+
+        [TestMethod]
+        public void RGB_SetColours_SetsCorrectValues()
+        {
+            RGB rgb = new RGB();
+            rgb.SetColours(255, 128, 64);
+
+            Assert.AreEqual(255, rgb.Red);
+            Assert.AreEqual(128, rgb.Green);
+            Assert.AreEqual(64, rgb.Blue);
+        }
+
+        [TestMethod]
+        public void RGBW_SetDMXData_SetsCorrectValues()
+        {
+            RGBW rgbw = new RGBW();
+            byte[] data = { 255, 128, 64, 32 };
+
+            rgbw.SetDMXData(data);
+
+            Assert.AreEqual(255, rgbw.Red);
+            Assert.AreEqual(128, rgbw.Green);
+            Assert.AreEqual(64, rgbw.Blue);
+            Assert.AreEqual(32, rgbw.White);
+        }
+
+        [TestMethod]
+        public void GRB_GetDMXData_ReturnsCorrectValues()
+        {
+            GRB grb = new GRB();           
+            grb.ConvertColour(255, 128, 64);
+
+            byte[] expectedData = { 128, 255, 64 };
+            CollectionAssert.AreEqual(expectedData, grb.GetDMXData());
         }
     }
 }
